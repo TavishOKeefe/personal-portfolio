b390887556a961374fd4f362ed52d4c61a7c52ed
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-15 15:22:55 -0800

    still researching possiblities for page layout and dynamic rendering.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 function Main() {
   return (
     <div>
-    <style jsx>{`
-      div {
-        border: 1px solid #000;
-        text-align: center;
-      },
-      `}</style>
     </div>
   );
 }
