ebef1d9c157a69f78b014721ae2d88e3749cb7aa
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-15 08:22:53 -0800

    Initial commit. Build basic React template for production.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App(){
+  return (
+    <div>
+      // <style jsx>{`
+      //   div {
+      //     border: 1px solid #000;
+      //   }
+      //   `}</style>
+      <h1>App is up and running.</h1>
+    </div>
+  );
+}
+
+export default App;
