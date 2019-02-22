54aed0d81d82ccca7d1aa76262c66579fdd3057f
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 09:06:54 -0800

    add images for hobbies.

--- src/components/experiences/personal/Hobbies.jsx
+++ src/components/experiences/personal/Hobbies.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import books from '../../../../src/assets/images/Books.jpg';
+import chess from '../../../../src/assets/images/Chess.jpg';
+import guitar from '../../../../src/assets/images/Guitar.jpg';
+import snowboard1 from '../../../../src/assets/images/Snowboard1.jpg';
 
 function Hobbies(){
   return (
     <div>
-      <p>This is where my Hobbies will go</p>
+      <img src={guitar}/>
+      <img src={snowboard1}/>
+      <img src={books}/>
+      <img src={chess}/>
     </div>
   );
 }
