c9e33bfa4163e8929058aa24e446de4c645fbb4d
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 10:27:25 -0800

    add information to contact page.

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 function Information() {
   return (
     <div>
-      <p>This is where my contact information will go</p>
+    <h3>Contact me anytime:</h3>
+      <p><strong>Email:</strong></p>
+      <p>okeefe.tavish@gmail.com</p>
+      <p><strong>Cell:</strong></p>
+      <p>405-219-8772</p>
+      <p><strong>Github Account:</strong></p>
+      <p>https://github.com/TavishOKeefe</p>
+      <p><strong>Linkedin Account:</strong></p>
+      <p>https://www.linkedin.com/in/tavishokeefe/</p>
+      <p><strong>Address:</strong></p>
+      <p>1944 NW Johnson Street</p>
+      <p>Portland, OR</p>
     </div>
   );
 }
