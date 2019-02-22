5a4a1a79e8279550267ef48811c8664a414e1ae1
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 10:01:49 -0800

    add synopsis and link to a book I wrote.

--- src/components/experiences/creative/writing/BigPondFish.jsx
+++ src/components/experiences/creative/writing/BigPondFish.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 function BigPondFish(){
   return (
     <div>
-      <p>This is where my BigPondFish will go</p>
+      <h3>My Writing</h3>
+      <h4>Big Pond Fish</h4>
+      <p>Big Pond Fish is a gritty novel set in New York City, recent past, which follows the life of Dillinger, a mid twenties renegade, who&apos;s apprehension at joining the corporate world, causes him to fall into a life of drug distribution and organized crime.</p><br/>
+      <p>Dillinger is introduced to the nightlife of New York City, and the money which can be made by satisfying the ravenous hunger of the youth&apos;s quest for distractions. But, there is a mysterious nature to his encounters, and he is in a constant state of question. He begins to drown his anxiety with intoxicants, and gains temporary relief. However, as time progresses, the Shadows, personifications of his growing addictions, and a bridge between the physical and metaphysical word, begin to take form, and affect not only his business, but his perception of reality.</p><br/>
+      <p> Big Pond Fish glamorizes entertainment, material possessions, and everything used to fill the voids carried by the lost. It takes the reader to the pinnacle of mass desire, then reveals the frivolity, and darkness which surrounds indulgence.</p><br/>
+      <p>Grounded through intellectual conversations, progressed by moments of confrontation, and captivating through touches of the surreal, Big Pond Fish is a thrilling read, with an ending no one will expect.
+      </p><br/>
+      <p><strong>Read full book here:</strong></p>
+      <p>https://docs.google.com/document/d/1qS5FXTrIsRg508gLCvmHV3dEFemPPOFefd7lWdaFMl4/edit?usp=sharing</p>
     </div>
   );
 }
