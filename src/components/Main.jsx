c2089d8f7c29063fe62832aac433217f9e85716d
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 10:22:27 -0800

    added an image and short bio pitch to main page. Edited layout of book query.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import profile from '../../src/assets/images/Profile.jpg';
 
 function Main() {
   return (
     <div>
+      <img src={profile}/>
+      <p>A highly focused, result-oriented, team player, with an established performance record in educational and operational environments. Expert in public speaking, presentations, and international integration. Demonstrated success with C#, JavaScript, React, and Angular computer languages. Continuing to master skills in software development, people management, finance, and chess.</p>
+      <p>I have lived and worked in many diverse cities, including; Los Angeles, New York City, Paris, and Portland. I speak basic French and Spanish, and have a proclivity for math, music, and the written word.</p>
     </div>
   );
 }
