c85015f15dc479bc14717ae127033a845767a244
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 08:44:00 -0800

    add links to Github projects.

--- src/components/experiences/professional/Academic.jsx
+++ src/components/experiences/professional/Academic.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function Academic(){
   return (
     <div>
-      <p>This is where my Academic will go</p>
+      <p><strong>Here is a link to my Github:</strong></p>
+      <p>https://github.com/TavishOKeefe</p>
+      <p><strong>Here are a few of the applications I built while attending Epicodus:</strong></p>
+      <p>https://github.com/TavishOKeefe/Super-Galactic-Age-Calculator</p>
+      <p>https://github.com/TavishOKeefe/Doctor-Lookup</p>
+      <p>https://github.com/TavishOKeefe/Restaurant-Roulette</p>
+      <p>https://github.com/TavishOKeefe/SocialMediaGame_Main</p>
     </div>
   );
 }
