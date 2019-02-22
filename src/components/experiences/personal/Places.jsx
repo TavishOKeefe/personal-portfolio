b7d679b01034516f09883f84bf2d614d140dbd81
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 15:24:56 -0800

    continue to try to create state in a way which is relevant to site.

--- src/components/experiences/personal/Places.jsx
+++ src/components/experiences/personal/Places.jsx
@@ -8,10 +8,6 @@ import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { v4 } from 'uuid';
 
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import classnames from 'classnames';
-import Collapse from '@material-ui/core/Collapse';
-
 import oklahoma from '../../../../src/assets/images/Oklahoma.jpg';
 import la from '../../../../src/assets/images/LA.jpg';
 import nyc from '../../../../src/assets/images/NYC.jpg';
@@ -37,104 +33,68 @@ const styles = theme => ({
     background:
       'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  media: {
-    height: 140,
-  }
 });
 
 const tileData = [
   {
     img: oklahoma,
     title: 'Oklahoma',
+    about: 'this is a test',
     id: v4(),
   },
   {
     img: la,
     title: 'Los Angeles',
+    about: 'this is a test',
     id: v4(),
   },
   {
     img: nyc,
     title: 'New York City',
+    about: 'this is a test',
     id: v4(),
   },
   {
     img: paris,
     title: 'Paris',
+    about: 'this is a test',
     id: v4(),
   },
   {
     img: portland,
     title: 'Portland',
+    about: 'this is a test',
     id: v4(),
   },
 ];
 
-class Places extends React.Component {
+function Places(props) {
+  const { classes } = props;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false
-    };
-    this.handleExpandClick =
-   this.handleExpandClick.bind(this);
-  }
-
-  handleExpandClick(){
-    if (this.state.expanded === false){
-      this.setState({expanded: true});
-    } else {
-      this.setState({expanded: false});
-    }
-  }
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <h2>Places:</h2>
-        <GridList className={classes.gridList} cols={2.5}>
-          {tileData.map(tile => (
-            <GridListTile key={tile.img}>
-              <img src={tile.img} alt={tile.title} />
-              <GridListTileBar
-                title={tile.title}
-                classes={{
-                  root: classes.titleBar,
-                  title: classes.title,
-                }}
-                actionIcon={
-                  <IconButton
-                    className={classnames(classes.expand, {[classes.expandOpen]: this.state.expanded,})}
-                    onClick={this.handleExpandClick}
-                    aria-expanded={this.state.expanded}>
-                    <ExpandMoreIcon />
-                  </IconButton>
-                }
-                <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-
-                    <p>It is Working.</p>
-
-                </Collapse>
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
-    );
-  }
+  return (
+    <div className={classes.root}>
+      <h2>Places:</h2>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
 
 Places.propTypes = {
