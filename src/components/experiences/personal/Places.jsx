4a6dea2860dfca267512eaf5c80baa53f98ac8b1
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-22 09:35:27 -0800

    add images to places.

--- src/components/experiences/personal/Places.jsx
+++ src/components/experiences/personal/Places.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import oklahoma from '../../../../src/assets/images/Oklahoma.jpg';
+import la from '../../../../src/assets/images/LA.jpg';
+import nyc from '../../../../src/assets/images/NYC.jpg';
+import paris from '../../../../src/assets/images/Paris.jpg';
+import portland from '../../../../src/assets/images/Portland.jpg';
 
 function Places(){
   return (
     <div>
-      <p>This is where my Places will go</p>
+      <h3>Life is a journey:</h3>
+      <img src={oklahoma}/>
+      <img src={la}/>
+      <img src={nyc}/>
+      <img src={paris}/>
+      <img src={portland}/>
     </div>
   );
 }
